581d3c3b953b145f0e52641579769dea7619a4ce
Evgeniya M <heatshepset@mail.ru>
2022-06-02 09:50:15 +0700

    first commit

---
+++ src/StringUtils.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace StringUtils;
+
+function capitalize($text)
+{
+    if ($text === '') {
+        return '';
+    }
+
+    $firstSymbol = strtoupper($text[0]);
+    $restSubstring = substr($text, 1);
+
+    return "{$firstSymbol}{$restSubstring}";
+}
